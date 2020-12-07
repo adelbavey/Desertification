de84ec4d44b7240800bfa35f72684a68e792592b
NinaNok <45494544+NinaNok@users.noreply.github.com>
2020-12-07 15:12:42 +0100

    copied HDRP

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public GameObject cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}

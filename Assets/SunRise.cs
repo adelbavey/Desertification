616edf658ee6d92c59d989e2449e6c334d1860a7
RabaDabaDoba <robindah@kth.se>
2020-12-10 17:27:41 +0100

    Update
    Push before Ninas audio

---
+++ Assets/SunRise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentRot = transform.rotation.eulerAngles;
+        currentRot.x += 0.01f;
+        Quaternion rot = Quaternion.Euler(currentRot);
+        transform.rotation = rot;
+
+    }
+}

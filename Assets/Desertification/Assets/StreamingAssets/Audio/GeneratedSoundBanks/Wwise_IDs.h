2a324a5f841e2e66b7fb88aefca6eada3eed906e
RabaDabaDoba <robindah@kth.se>
2020-12-11 15:25:22 +0100

    Working run/walk sound

--- Assets/Desertification/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/Desertification/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -28,6 +28,7 @@ namespace AK
 
             namespace SWITCH
             {
+                static const AkUniqueID GOLV = 651855293U;
                 static const AkUniqueID GRASS = 4248645337U;
                 static const AkUniqueID SAND = 803837735U;
                 static const AkUniqueID WOOD = 2058049674U;

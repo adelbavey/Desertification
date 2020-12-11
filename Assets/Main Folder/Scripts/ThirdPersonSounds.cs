a494c19a8138835649a182375c80848aac17ef72
NinaNok <45494544+NinaNok@users.noreply.github.com>
2020-12-11 12:45:32 +0100

    testing again

---
+++ Assets/Main Folder/Scripts/ThirdPersonSounds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonSounds : MonoBehaviour
+{
+
+    Animator m_Animator;
+
+    //public AK.Wwise.Switch MySwtich;
+
+    private bool jump;
+    public AK.Wwise.Event FootStepEvent;
+    public AK.Wwise.Switch MySwitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FootStepPlay(){
+        MySwitch.SetValue(gameObject);
+        //AkSoundEngine.PostEvent("footstep_player", gameObject);
+        FootStepEvent.Post(gameObject);
+    }
+
+    public void JumpPlay(){
+        AkSoundEngine.PostEvent("jump_player", gameObject);
+    }
+}
---
+++ Assets/SunRise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentRot = transform.rotation.eulerAngles;
+        Debug.Log(currentRot);
+        if(currentRot.x <= 25.0f || currentRot.x > 335){
+            currentRot.x -= 0.05f;
+        }
+        
+
+        Quaternion rot = Quaternion.Euler(currentRot);
+        transform.rotation = rot;
+
+    }
+}

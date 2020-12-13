534024e68537b1d0b915e963f500cf6c69b9bd55
RabaDabaDoba <robindah@kth.se>
2020-12-13 20:03:01 +0100

    Update

--- Assets/Main Folder/Scripts/PaintableTerrain.cs
+++ Assets/Main Folder/Scripts/PaintableTerrain.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class PaintableTerrain : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Texture2D[] TerrainTextures;
+    public TerrainData terraindata;
     public Terrain terrain;
 	public Camera main;
+
+    public Material material;
+    
+
 
 
 // Use this for initialization
@@ -18,8 +24,28 @@ void Start()
 
 }
 
-private void Update() {
-	Debug.Log(terrain.terrainData.GetHeight((int)main.transform.position.x, (int)main.transform.position.z));
+void FixedUpdate() {
+    Vector3 pos = main.transform.position;
+    int x = Mathf.CeilToInt(pos.x);
+    int z = Mathf.CeilToInt(pos.z);
+	Debug.Log(terrain.terrainData.GetHeight(x,z));
+
+    
+    if(Input.GetKey(KeyCode.G)){
+        
+    }
+    
 }
+
+ public void CreateTerrain(){
+                 SplatPrototype[] tex = new SplatPrototype [TerrainTextures.Length];
+                 for (int i=0; i<TerrainTextures.Length; i++) {
+                         tex [i] = new SplatPrototype ();
+                         tex [i].texture = TerrainTextures [i];    //Sets the texture
+                         tex [i].tileSize = new Vector2 (1, 1);    //Sets the size of the texture
+                 }
+                 terraindata.splatPrototypes = tex;
+                 terrain = Terrain.CreateTerrainGameObject (terraindata).GetComponent<Terrain> ();
+     }
 
 }

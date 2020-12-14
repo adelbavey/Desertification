68096e8e158a922f21e58b758c71e867c7efbc76
RabaDabaDoba <robindah@kth.se>
2020-12-14 23:59:05 +0100

    Update GrowShit.cs

--- Assets/GrowShit.cs
+++ Assets/GrowShit.cs
@@ -13,6 +13,10 @@ public class GrowShit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 temp = transform.localScale;
+        temp.x += 0.05f;
+        temp.y += 0.05f;
+        temp.z += 0.05f;
+        transform.localScale = temp; 
     }
 }

44eb539af00ccd59d7c8455b11158de6c2e9bc9d
RabaDabaDoba <robindah@kth.se>
2020-12-14 15:33:52 +0100

    Added LOD for trees

--- Assets/GameProgression.cs
+++ Assets/GameProgression.cs
@@ -10,6 +10,9 @@ public class GameProgression : MonoBehaviour
     bool reachedOasis = false;
     bool takenOrb = false;
 
+    public GameObject Desert;
+    public GameObject Forest;
+
     void Start()
     {
         Debug.Log("Starting GameProgression");
@@ -32,7 +35,35 @@ public class GameProgression : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider other) {
-        Debug.Log(other.tag);
+        
+        
+        //Picking up item at camp
+        if(other.tag == "SpecialItem"){
+            Debug.Log("asda");
+            other.gameObject.active = false;
+            //AkSoundEngine.PostEvent("pick_up_item_player", gameObject);
+            takenOrb = true;
+        }
+
+        //Going back to the oasis with orb
+        if(other.gameObject.name == "BridgeTrigger" && takenOrb){
+            Debug.Log("yaaay");
+            levelup();
+
+        }
+
+    }
+
+
+    void levelup(){
+        //Throw the object in water
+
+        //Trigger cutscene with growing forest
+
+        //Change to forest environment.
+        Desert.SetActive(false);
+        Forest.SetActive(true);
+
     }
 
 }
